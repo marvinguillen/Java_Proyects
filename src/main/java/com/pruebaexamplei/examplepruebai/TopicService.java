018ee4517e49b9c9f390a1b2f084dd3c026cee2d
unknown <marvin.a.guillen@gmail.com>
2021-01-03 02:48:26 -0600

    second commit

--- src/main/java/com/pruebaexamplei/examplepruebai/TopicService.java
+++ src/main/java/com/pruebaexamplei/examplepruebai/TopicService.java
@@ -22,7 +22,19 @@ public class TopicService {
     {
        return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
     }
-	public void add(Topic topic) {
+	public void addTopic(Topic topic) {
         topics.add(topic);
+	}
+	public void updateTopic(String id,Topic topic) {
+        for(int i=0; i< topics.size();i++){
+            Topic t =topics.get(i);
+            if(t.getId().equals(id)){
+                topics.set(i,topic);
+                return;
+            }
+        }
+	}
+	public void  deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
